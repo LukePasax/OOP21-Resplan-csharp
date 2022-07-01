abb75ef0ea3033b1e4c05f90aac05363df18eb38
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-01 18:22:37 +0200

    Add ISection with doc

---
+++ Resplan/GabrieleMenghi/Project/ISection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Resplan.Pasini.Part;
+
+namespace Resplan.GabrieleMenghi.Project
+{
+    /// <summary>
+    /// It's an interface modelling a section, that is a named part of the timeline
+    /// </summary>
+    interface ISection : IElement
+    {
+        /// <summary>
+        /// Return the duration of the section
+        /// </summary>
+        double Duration { get; }
+    }
+}

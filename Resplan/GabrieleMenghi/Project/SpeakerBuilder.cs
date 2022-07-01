0dab38c35f28b41abca1e639ef167c6ee34b107f
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-01 16:15:52 +0200

    Add doc in SpeakerBuilder.cs

--- Resplan/GabrieleMenghi/Project/SpeakerBuilder.cs
+++ Resplan/GabrieleMenghi/Project/SpeakerBuilder.cs
@@ -2,29 +2,39 @@
 
 namespace Resplan.GabrieleMenghi.Project
 {
+    /// <summary>
+    /// This is the implementation of the builder for an ISpeaker
+    /// </summary>
     class SpeakerBuilder : ISpeakerBuilder
     {
         private int _speakerCode;
-        private string _firstName = null;
-        private string _lastName = null;
+        private string? _firstName = null;
+        private string? _lastName = null;
 
+        /// <summary>
+        /// First part of the builder
+        /// </summary>
+        /// <param name="speakerCode">the code associated to the speaker</param>
         public SpeakerBuilder(int speakerCode)
         {
             this._speakerCode = speakerCode;
         }
 
+        /// <inheritdoc/>
         public ISpeakerBuilder FirstName(string firstName)
         {
             this._firstName = firstName;
             return this;
         }
 
+        /// <inheritdoc/>
         public ISpeakerBuilder LastName(string lastName)
         {
             this._lastName = lastName;
             return this;
         }
 
+        /// <inheritdoc/>
         public ISpeaker build()
         {
             if(this._firstName == null || this._lastName == null)

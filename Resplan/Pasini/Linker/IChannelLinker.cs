6910b1edec52088c5bd389f90030f86be5ba524e
Luca Pasini <pasini.luca09@gmail.com>
2022-07-01 16:09:38 +0200

    Add interface IChannelLinker

---
+++ Resplan/Pasini/Linker/IChannelLinker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Resplan.Sirri;
+using Resplan.Sirri.Role;
+
+namespace Resplan.Pasini.Linker
+{
+    public interface IChannelLinker
+    {
+        void AddChannelReferences(IRole role, IChannel channel);
+        
+        IChannel GetChannel(IRole role);
+        
+        IRole GetRole(IChannel channel);
+        
+        IRole GetRoleFromTitle(string title);
+        
+        ISet<IRole> getRolesSet(IRole.RoleType type);
+        
+        bool ChannelExists(string channelTitle);
+        
+    }
+}

09e727b1ebd7b25882925fe4a134e1b357c748c3
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-01 12:46:38 +0200

    Add ISpeakerBuilder implementation

---
+++ Resplan/GabrieleMenghi/Project/SpeakerBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Resplan.GabrieleMenghi.Project
+{
+    class SpeakerBuilder : ISpeakerBuilder
+    {
+        private int _speakerCode;
+        private string _firstName;
+        private string _lastName;
+
+        public SpeakerBuilder(int speakerCode)
+        {
+            this._speakerCode = speakerCode;
+        }
+
+        public ISpeakerBuilder FirstName(string firstName)
+        {
+            this._firstName = firstName;
+            return this;
+        }
+
+        public ISpeakerBuilder LastName(string lastName)
+        {
+            this._lastName = lastName;
+            return this;
+        }
+
+        public ISpeaker build()
+        {
+            if(this._firstName == null || this._lastName == null)
+            {
+                throw new MissingFieldException("Null parameters");
+            }
+            return new Speaker(this._speakerCode, this._firstName, this._lastName);
+        }
+    }
+}

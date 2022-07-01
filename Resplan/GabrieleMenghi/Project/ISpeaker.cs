5fa1169edad22ac98da00ce8bc5f053cb8070758
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-01 11:56:24 +0200

    Add interface ISpeaker

---
+++ Resplan/GabrieleMenghi/Project/ISpeaker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace Resplan.GabrieleMenghi.Project
+{
+    public interface ISpeaker
+    {
+        int SpeakerCode { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}

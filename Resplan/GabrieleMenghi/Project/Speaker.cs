0eac4661cc1d42a3eedb16baf2fb54f9b91b2c38
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-01 16:10:29 +0200

    Add doc in Speaker.cs

--- Resplan/GabrieleMenghi/Project/Speaker.cs
+++ Resplan/GabrieleMenghi/Project/Speaker.cs
@@ -1,13 +1,25 @@
 ﻿namespace Resplan.GabrieleMenghi.Project
 {
+    /// <summary>
+    /// This is a simple implementation of an ISpeaker
+    /// </summary>
     class Speaker : ISpeaker
     {
+        /// <inheritdoc/>
         public int SpeakerCode { get; private set; }
 
+        /// <inheritdoc/>
         public string FirstName { get; private set; }
 
+        /// <inheritdoc/>
         public string LastName { get; private set; }
 
+        /// <summary>
+        /// Build a speaker
+        /// </summary>
+        /// <param name="speakerCode">the code associated to the speaker</param>
+        /// <param name="firstName">the first name of the speaker</param>
+        /// <param name="lastName">the last name of the speaker</param>
         public Speaker(int speakerCode, string firstName, string lastName)
         {
             SpeakerCode = speakerCode;
@@ -15,6 +27,7 @@
             LastName = lastName;
         }
 
+        /// <inheritdoc/>
         public override string ToString() => $"S {SpeakerCode}, {FirstName}, {LastName}";
     }
 }

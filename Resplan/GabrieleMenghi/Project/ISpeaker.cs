7a3e492bf7dcd4d9f76bb8d08f44d6887d9a76bb
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-01 12:18:02 +0200

    Add ISpeaker implementation

--- Resplan/GabrieleMenghi/Project/ISpeaker.cs
+++ Resplan/GabrieleMenghi/Project/ISpeaker.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-
-namespace Resplan.GabrieleMenghi.Project
+﻿namespace Resplan.GabrieleMenghi.Project
 {
     /// <summary>
     /// This interface represents a speaker who can participate in a project
---
+++ Resplan/GabrieleMenghi/Project/Speaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resplan.GabrieleMenghi.Project
+{
+    class Speaker : ISpeaker
+    {
+        public int SpeakerCode { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public Speaker(int speakerCode, string firstName, string lastName)
+        {
+            SpeakerCode = speakerCode;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString() => $"S {SpeakerCode}, {FirstName}, {LastName}";
+    }
+}

80aa5d28e8565c35724a80d85217a42f5b410b43
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-07-01 12:02:51 +0200

    Add doc to ISpeaker

--- Resplan/GabrieleMenghi/Project/ISpeaker.cs
+++ Resplan/GabrieleMenghi/Project/ISpeaker.cs
@@ -3,10 +3,22 @@ using System.Text;
 
 namespace Resplan.GabrieleMenghi.Project
 {
+    /// <summary>
+    /// This interface represents a speaker who can participate in a project
+    /// </summary>
     public interface ISpeaker
     {
+        /// <summary>
+        /// The code representing the speaker
+        /// </summary>
         int SpeakerCode { get; }
+        /// <summary>
+        /// The first name of the speaker
+        /// </summary>
         string FirstName { get; }
+        /// <summary>
+        /// The last name of the speaker
+        /// </summary>
         string LastName { get; }
     }
 }

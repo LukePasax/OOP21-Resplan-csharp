6263427660308fe36192abbc8d30c79589dd94fd
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-07-04 18:53:02 +0200

    Add test for channel factory

--- Resplan/Sirri/Test/TestChannel.cs
+++ Resplan/Sirri/Test/TestChannel.cs
@@ -11,6 +11,7 @@ namespace Resplan.Sirri.Test
         [Test]
         public void TestVolume()
         {
+            Assert.AreEqual(100, Channel.Volume);
             // Volume must be a value between 0 and 100.
             Channel.Volume = 50;
             Assert.AreEqual(50, Channel.Volume);
@@ -28,6 +29,18 @@ namespace Resplan.Sirri.Test
             Assert.IsFalse(Channel.Enabled);
             Channel.Enable();
             Assert.IsTrue(Channel.Enabled);
+        }
+
+        [Test]
+        public void TestFactory()
+        {
+            IChannelFactory cf = new ChannelFactory();
+            var audio = cf.Audio();
+            Assert.AreEqual(IChannel.ChannelType.Audio, audio.Type);
+            Assert.AreEqual("Compressor", audio.ProcessingUnit?.GetEffectAtPosition(0).Name);
+            var master = cf.Master();
+            Assert.AreEqual(IChannel.ChannelType.Master, master.Type);
+            Assert.AreEqual("High Pass", master.ProcessingUnit?.GetEffectAtPosition(1).Name);
         }
     }
 }
